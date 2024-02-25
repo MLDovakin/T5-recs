2bca0224dd42fbb9a6c5dc1ec883358e06c7ca92
Dragonborn <78375175+MLDovakin@users.noreply.github.com>
2024-02-25 18:04:57 +0300

    Update model_T5.py

--- model_T5.py
+++ model_T5.py
@@ -7,36 +7,18 @@ import torch
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from peft import PromptEmbedding, PromptTuningConfig, get_peft_model
+from peft import prepare_model_for_int8_training
 
 tokenizer = AutoTokenizer.from_pretrained("t5-base")
-model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
+model = AutoModelForSeq2SeqLM.from_pretrained("t5-base",load_in_8bit=True)
+model = prepare_model_for_int8_training(model)
 
-config = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    lora_dropout=0.05,
-    bias="none",
-    task_type="CAUSAL_LM"
-)
-config.inference_mode = False
 
-model = get_peft_model(model, config)
-
-config = PromptTuningConfig(
-    peft_type="PROMPT_TUNING",
-    task_type="SEQ_2_SEQ_LM",
-    num_virtual_tokens=20,
-    token_dim=768,
-    num_transformer_submodules=1,
-    num_attention_heads=12,
-    num_layers=12,
-    prompt_tuning_init="TEXT",
-    prompt_tuning_init_text="History : {user_pusrchases} Candidates for recommendations {candidates} Reccomendadion: {output predict}",
-    tokenizer_name_or_path="t5-base",
+lora_config = LoraConfig(
+    r=16, lora_alpha=32, target_modules=["q", "v"], lora_dropout=0.05, bias="none", task_type="SEQ_2_SEQ_LM"
 )
 
-for name, param in model.named_parameters():
-    if 'embed' in name:
-      param.requires_grad = False
 
-model = get_peft_model(model, config)
+model = get_peft_model(model, lora_config)
+
+print_trainable_parameters(model)
